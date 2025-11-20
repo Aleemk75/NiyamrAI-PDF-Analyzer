a2c1526e9c8dcec66b7e6f84fad467b3091bf8fd
AleemK75 <ak2514905@gmail.com>
2025-11-21 02:48:56 +0530

    complete initial version of PDF Compliance Checker app

---
+++ server/src/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up EJS as view engine
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+import checkRouter from './routes/check.route.js';
+app.use(express.json());
+
+app.use(cors());
+
+// Home route
+app.get('/', (req, res) => {
+  res.render('home');
+});
+app.get("/results", (req, res) => {
+    res.render("results");   // results.ejs in views folder
+});
+app.use('/api/check', checkRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
